048c0391efb65faa44a12cfa29faa235dec72238
ccat <ccathlete01@gmail.com>
2025-02-28 22:00:18 +0100

    Implemented the like_a_tweet use case.

--- src/infrastructure/api_clients/twitter/api_client.py
+++ src/infrastructure/api_clients/twitter/api_client.py
@@ -1,4 +1,23 @@
+import time
+from tweepy import OAuth1UserHandler, API, Tweet, errors  # type: ignore
 from src.domain.entities.twitter import Tweet, Id
+from typing_extensions import TypeAlias
+
+
+EndpointInfo: TypeAlias = dict[str, int]
+"""
+Type: dict[str, int]
+"""
+Resource: TypeAlias = dict[str, EndpointInfo]
+"""
+Type: dict[str, EndpointInfo]
+"""
+ResourcesDict: TypeAlias = dict[str, Resource]
+"""
+Type: dict[str, Resource]\n
+Resource: dict[str, EndpointInfo]\n
+EndpointInfo: dict[str, int]\n
+"""
 
 
 class ApiClient:
@@ -6,8 +25,59 @@ class ApiClient:
     An object that represents a user of the Twitter API.
     """
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        consumer_key: str,
+        consumer_secret: str,
+        access_token: str,
+        access_token_secret: str,
+    ):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_token = access_token
+        self.access_token_secret = access_token_secret
+
+        # Authentication (OAuth 1.0) since it's easier to imitate a
+        # user.
+        self.auth: OAuth1UserHandler = OAuth1UserHandler(
+            consumer_key=self.consumer_key,
+            consumer_secret=self.consumer_secret,
+            access_token=self.access_token,
+            access_token_secret=self.access_token_secret,
+        )
+        self.api: API = API(self.auth)
+
+    def _is_above_rate_limit(
+        self,
+        resource_name: str,
+        endpoint_uri: str,
+    ) -> tuple[bool, int]:
+        """
+        Checking if we have reached the rate limit for a specific
+        resource and endpoint, returning a tuple (bool, int):
+        - bool: True if rate limit exceeded, False otherwise
+        - int: The reset time if rate limit exceeded, 0 if not
+        """
+        above_limit: bool = False
+        reset_time: int = 0
+        try:
+            # Contains informatino about status pf a specific
+            # resource.
+            info_json: ResourcesDict = self.api.rate_limit_status(
+                resources=[resource_name],
+            )
+            resource: Resource = info_json[resource_name]
+            endpoint: EndpointInfo = resource[endpoint_uri]
+            remaining_requests: int = endpoint["remaining"]
+            reset_time = endpoint["reset"]
+            print(f"Remaining requests for {resource_name}: {remaining_requests}")
+            if remaining_requests == 0:
+                above_limit = True
+
+        except errors.TweepyException as e:
+            print(f"Failed to get rate limit status: {e}.")
+
+        return above_limit, reset_time
 
     def like_tweet(self, tweet: Tweet) -> bool:
         """
@@ -30,7 +100,10 @@ class ApiClient:
         tweet_id: Id,
     ) -> dict[str, str]:
         """
-        Fetches the data of a tweet by ID from the twitter API(JSON).
+        Checking if we have reached the rate limit for a specific
+        resource and endpoint, returning a tuple (bool, int):
+        - bool: True if rate limit exceeded, False otherwise
+        - int: The reset time if rate limit exceeded, 0 if not
         """
         tweet_data: dict[str, str]
         return tweet_data
