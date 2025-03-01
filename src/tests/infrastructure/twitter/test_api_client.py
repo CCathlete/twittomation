9f78f3313e7777c5d4134b8a2b8aa90abe3a05c4
ccat <ccathlete01@gmail.com>
2025-03-01 21:41:11 +0100

    Created mock api client

--- src/tests/infrastructure/twitter/test_api_client.py
+++ src/tests/infrastructure/twitter/test_api_client.py
@@ -1,20 +1,43 @@
+import os
 import pytest
 import pytest_mock as ptm
 from src.domain.services.twitter.tweet_liking_service import TweetLikingService
 from src.infrastructure.api_clients.twitter import ApiClient
 
 
+# Setting up a fixture for reusability.
 @pytest.fixture
-def mock_api_client(mocker: ptm.MockerFixture) -> ptm.MockType:
-    """
-    Creates a mock for the api client and returns it.
-    """
-    mock_api: ptm.MockType = mocker.Mock(spec=ApiClient)
-    # Simulating the like-tweet method.
-    mock_api.like_tweet.return_value = True
-    mocker.patch(
-        "src.infrastructure.api_clients.twitter.ApiClient",
-        return_value=mock_api,
+def api_client() -> ApiClient:
+    return ApiClient(
+        consumer_key=os.getenv(
+            "TWITTER_CONSUMER_KEY",
+            default="",
+        ),
+        consumer_secret=os.getenv(
+            "TWITTER_CONSUMER_SECRET",
+            default="",
+        ),
+        access_token=os.getenv(
+            "TWITTER_ACCESS_TOKEN",
+            default="",
+        ),
+        access_token_secret=os.getenv(
+            "TWITTER_ACCESS_TOKEN_SECRET",
+            default="",
+        ),
     )
 
-    return mock_api
+
+@pytest.fixture
+def mock_api_client(mocker) -> ptm.MockType:
+    mock_client = mocker.Mock(spec=ApiClient)
+    mock_client.like_tweet.return_value = True  # Simulating a
+    # successful like.
+
+    # Setting the constructor to return the mock object instead of
+    # the real one.
+    mocker.patch(
+        "src.infrastructure.api_clients.twitter.api_client.ApiClient",
+        return_value=mock_client,
+    )
+    return mock_client
